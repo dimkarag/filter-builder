e0746b7900b5ba38f4eb86aabb14accd0cbe8875
Dimitrios Karageorgos <d.karageorgos@mpass.gr>
2021-10-10 01:36:50 +0300

    remove index file

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import FilterBuilder from '@/src/FilterBuilder'
-
-export default {
-    install(Vue, options) {
-        Vue.component("filter-builder", FilterBuilder);
-    }
-}
